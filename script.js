25d434fb7b983c8f200017ad91af886069047c98
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-11-30 19:34:28 -0800

    adding logic

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ let total = 0;
 let blackJack = false;
 let stillAlive = false;
 
+let myCards = document.getElementById('myCards');
+let myTotal = document.getElementById('myTotal');
 
+// buttons
+let startGame = document.getElementById('start');
+
+
+// startGame functionality
+startGame.addEventListener('click', beginGame);
 // start by grabbing a random card
 function getRandomCard() {
     // use math.random
@@ -20,5 +28,20 @@ function getRandomCard() {
 }
 
 function beginGame() {
-    
+    stillAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    total = firstCard + secondCard;
+    displayGame();
 }
+
+function displayGame() {
+    myCards.textContent = 'Cards: '
+    for (let i = 0; i < cards.length; i++) {
+        myCards.textContent += cards[i] + ' ';
+    }
+    myTotal.textContent = total
+}
+
+
