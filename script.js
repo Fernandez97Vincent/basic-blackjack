63e8b8efb28982b2e02c4e3bdfee9219c67c154d
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-11-30 19:47:42 -0800

    finished player side, want to add an opponent and credits

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ let stillAlive = false;
 
 let myCards = document.getElementById('myCards');
 let myTotal = document.getElementById('myTotal');
-
+let msg = document.getElementById('msg');
 // buttons
 let startGame = document.getElementById('start');
+let newCard = document.getElementById('new-card');
+let newGame = document.getElementById('new-game');
 
-
+//new game
+newGame.addEventListener('click', beginGame);
+// new card
+newCard.addEventListener('click', getNewCard);
 // startGame functionality
 startGame.addEventListener('click', beginGame);
 // start by grabbing a random card
@@ -27,6 +32,15 @@ function getRandomCard() {
     }
 }
 
+function getNewCard() {
+    if (stillAlive === true && blackJack === false) {
+        let card = getRandomCard()
+        total += card;
+        cards.push(card);
+        displayGame();
+    }
+}
+
 function beginGame() {
     stillAlive = true;
     let firstCard = getRandomCard();
@@ -41,7 +55,18 @@ function displayGame() {
     for (let i = 0; i < cards.length; i++) {
         myCards.textContent += cards[i] + ' ';
     }
-    myTotal.textContent = total
+    myTotal.textContent = "Total: " + total
+    if (total < 21) {
+        msg.textContent = "Do you want to draw another card"
+        blackJack = false;
+    }
+    else if (total === 21) {
+        msg.textContent = "Blackjack! Do you want to play again?"
+        blackJack = true;
+    }
+    else {
+        msg.textContent = "You lose, try again";
+    }
 }
 
 
